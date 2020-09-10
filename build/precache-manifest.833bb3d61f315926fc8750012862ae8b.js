be0dd97b7e8aeba1d8be25a422beb7bafd6137e0
Sharad <you@example.com>
2020-09-10 14:08:42 +0300

    Build file published

---
+++ build/precache-manifest.833bb3d61f315926fc8750012862ae8b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a4defd38347efbb7f05a6a4c13b26a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c54e741fd8cef4d8ebd6",
+    "url": "/static/css/main.7bdf0d04.chunk.css"
+  },
+  {
+    "revision": "e935b36b56a3042902d3",
+    "url": "/static/js/2.ea5ee727.chunk.js"
+  },
+  {
+    "revision": "fcf4f6d3b886ef447aa7d2c266f2141a",
+    "url": "/static/js/2.ea5ee727.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c54e741fd8cef4d8ebd6",
+    "url": "/static/js/main.b69e0532.chunk.js"
+  },
+  {
+    "revision": "25f4ef40237fb0a9b6e8",
+    "url": "/static/js/runtime-main.0b269949.js"
+  },
+  {
+    "revision": "78313ace4e54623190f1ee1b59a24058",
+    "url": "/static/media/logo.78313ace.svg"
+  }
+]);
