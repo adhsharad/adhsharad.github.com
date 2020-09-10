44ffc21de4e0acc266db8c21504f5bdb414066c6
Purr Consuegra <purr.consuegra@solita.fi>
2020-09-10 14:37:50 +0300

    Retry with different folder

---
+++ build/precache-manifest.adc9c8db837ebe8676c2b5dca171c15c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1fb80406de876f3287cd5b86851cfcd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7af79ca9d2b6eb3514b",
+    "url": "/static/css/main.7bdf0d04.chunk.css"
+  },
+  {
+    "revision": "e935b36b56a3042902d3",
+    "url": "/static/js/2.ea5ee727.chunk.js"
+  },
+  {
+    "revision": "fcf4f6d3b886ef447aa7d2c266f2141a",
+    "url": "/static/js/2.ea5ee727.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7af79ca9d2b6eb3514b",
+    "url": "/static/js/main.26801945.chunk.js"
+  },
+  {
+    "revision": "25f4ef40237fb0a9b6e8",
+    "url": "/static/js/runtime-main.0b269949.js"
+  },
+  {
+    "revision": "78313ace4e54623190f1ee1b59a24058",
+    "url": "/static/media/logo.78313ace.svg"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/build/precache-manifest.08049e4c163eb72d21416ec3795b5b4e.js"
+  "/precache-manifest.adc9c8db837ebe8676c2b5dca171c15c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/build/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
